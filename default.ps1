a5c668c1d8d4491d49af6f3cc24168afd935851b
Chris Duck <gpduck@whatsupduck.net>
2015-08-30 19:28:21 -0500

    add text again

---
+++ default.ps1
@@ -0,0 +1,16 @@
+﻿
+Task default -Depends build
+
+Task build -Depends sass,Jekyll {
+	
+}
+
+Task sass {
+	sass .\sass\site_bootstrap.scss .\css\site_bootstrap.css
+}
+
+Task Jekyll {
+	jekyll build --config "_config.yml,_configLocal.yml" --destination .\_site\
+}
+
+asdf
